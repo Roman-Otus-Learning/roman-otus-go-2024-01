eb50ee94774142282d8cba32e5c7b3bd43475a56
Боровых Роман Игоревич <roman.borovykh@vseinstrumenti.ru>
2024-02-18 19:37:47 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,58 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputString string) (string, error) {
+	var stringBuilder strings.Builder
+	inputStringRunes := []rune(inputString)
+
+	for currentIndex, currentRune := range inputStringRunes {
+		isCurrentRuneDigit := unicode.IsDigit(currentRune)
+		if currentIndex == 0 && isCurrentRuneDigit {
+			return "", ErrInvalidString
+		}
+
+		nextRune, err := getRuneByIndex(inputStringRunes, currentIndex+1)
+		if err != nil && isCurrentRuneDigit {
+			continue
+		}
+
+		isNextRuneDigit := unicode.IsDigit(nextRune)
+		if isCurrentRuneDigit && isNextRuneDigit {
+			return "", ErrInvalidString
+		}
+
+		if isNextRuneDigit {
+			repeatCount, err := strconv.Atoi(string(nextRune))
+			if err != nil {
+				return "", err
+			}
+
+			stringBuilder.WriteString(repeatStringByRune(currentRune, repeatCount))
+		} else if !isCurrentRuneDigit {
+			stringBuilder.WriteString(string(currentRune))
+		}
+	}
+
+	return stringBuilder.String(), nil
+}
+
+func getRuneByIndex(inputStringRunes []rune, index int) (rune, error) {
+	inputStringLength := len(inputStringRunes)
+	if index >= inputStringLength {
+		return 0, fmt.Errorf("индекс превышает размер строки")
+	}
+
+	return inputStringRunes[index], nil
+}
+
+func repeatStringByRune(r rune, repeatCount int) string {
+	return strings.Repeat(string(r), repeatCount)
 }
