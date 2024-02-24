99aef101ca1097b453881a57314e17cfc04d1459
Боровых Роман Игоревич <roman.borovykh@vseinstrumenti.ru>
2024-02-24 13:52:19 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,62 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+var maxWordsCount = 10
+
+func Top10(incomingText string) []string {
+	if len(incomingText) == 0 {
+		return make([]string, 0)
+	}
+
+	explodedWords := getMapOfCountedWords(incomingText)
+	sortedWords := getSliceOfSortedWords(explodedWords)
+
+	return sortedWords[:getMaxWordsCount(sortedWords)]
+}
+
+func getMapOfCountedWords(incomingText string) map[string]int {
+	words := strings.Split(incomingText, " ")
+	countedWords := make(map[string]int)
+
+	for _, currentWord := range words {
+		if currentWord == "" {
+			continue
+		}
+
+		for _, explodedWords := range strings.Fields(currentWord) {
+			countedWords[explodedWords]++
+		}
+	}
+
+	return countedWords
+}
+
+func getSliceOfSortedWords(countedWords map[string]int) []string {
+	sortedWords := make([]string, 0, len(countedWords))
+	for word := range countedWords {
+		sortedWords = append(sortedWords, word)
+	}
+
+	sort.Slice(sortedWords, func(i, j int) bool {
+		if countedWords[sortedWords[i]] == countedWords[sortedWords[j]] {
+			return sortedWords[i] < sortedWords[j]
+		}
+
+		return countedWords[sortedWords[i]] > countedWords[sortedWords[j]]
+	})
+
+	return sortedWords
+}
+
+func getMaxWordsCount(words []string) int {
+	wordsCount := len(words)
+	if wordsCount < maxWordsCount {
+		maxWordsCount = wordsCount
+	}
+
+	return maxWordsCount
 }
