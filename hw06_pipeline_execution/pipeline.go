1704996c125ede4f6f258c5c51629aeff704dac8
Боровых Роман Игоревич <roman.borovykh@vseinstrumenti.ru>
2024-04-15 23:43:13 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,47 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if in == nil {
+		return nil
+	}
+
+	out := in
+	for _, stage := range stages {
+		out = executeStage(stage(out), done)
+	}
+
+	return out
+}
+
+func executeStage(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+			for len(in) > 0 {
+				<-in
+			}
+		}()
+
+		for data := range in {
+			if isTerminated(done) {
+				return
+			}
+
+			out <- data
+		}
+	}()
+
+	return out
+}
+
+func isTerminated(done In) bool {
+	if done != nil {
+		if _, ok := <-done; !ok {
+			return true
+		}
+	}
+
+	return false
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,32 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty stages", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]int, 0, 5)
+
+		var emptyStages []Stage
+
+		for s := range ExecutePipeline(in, nil, emptyStages...) {
+			result = append(result, s.(int))
+		}
+
+		require.Equal(t, data, result)
+	})
+
+	t.Run("in is nil", func(t *testing.T) {
+		result := ExecutePipeline(nil, nil, make([]Stage, 1)...)
+
+		require.Nil(t, result)
+	})
 }
